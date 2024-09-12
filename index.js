481be2c6cbe697899c6b6f4f1e8ecb468aacc42c
Rodrigo-Gabriel <rodrigobelchio@gmail.com>
2024-09-12 17:45:32 -0300

    resolve problema marcadas/desmarcadas

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ const listarMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if(respostas.length == 0) {
         console.log("Nenhuma meta selecionada!");
         return;
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
